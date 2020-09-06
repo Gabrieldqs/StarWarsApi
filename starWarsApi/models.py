dec777dcc83b6c8e2698d1fdeff184ebb1a17a84
Gabriel Delgado <gdelgado@funcionalcorp.com.br>
2020-09-06 15:50:23 -0300

    Revert "Initial commit"
    This reverts commit a6a0ce7afb6969d897fcb5df9f1447e0c79f9903.

--- starWarsApi/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models 
-
-class Planet(models.Model):
-	name = models.CharField(max_length=100)
-	climate = models.CharField(max_length=100)
-	terrain = models.CharField(max_length=100)
-	films = models.IntegerField()
